5bcb9fd9686022e58e53cd639b910ea3ba75e231
Paul Mason <paul.mason@outlook.com>
2023-02-07 09:21:09 +0200

    update

--- demos/theme/rollup.config.js
+++ demos/theme/rollup.config.js
@@ -1,16 +1,36 @@
 import merge from 'deepmerge';
 import { createSpaConfig } from '@open-wc/building-rollup';
+import { terser } from 'rollup-plugin-terser';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 
 const baseConfig = createSpaConfig({
-  developmentMode: process.env.ROLLUP_WATCH === 'true',
-  injectServiceWorker: false,
-  workbox: false
+    developmentMode: process.env.ROLLUP_WATCH === 'true',
+    injectServiceWorker: false,
+    workbox: false
 });
 
 export default merge(baseConfig, {
-  output: {
-    dir: '../dist/theme'
-  },
-  input: './index.html'
+    output: {
+        dir: '../dist/theme',
+        sourcemap: false,
+        plugins: [
+            terser({
+                ecma: 2020,
+                mangle: { toplevel: true },
+                compress: {
+                    module: true,
+                    toplevel: true,
+                    unsafe_arrows: true,
+                    drop_console: true,
+                    drop_debugger: true
+                },
+                output: { quote_style: 1 }
+            })
+        ]
+    },
+    input: './index.html',
+    plugins: [
+        minifyHTML()
+    ]
 });
 
